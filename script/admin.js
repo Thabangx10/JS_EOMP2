f0e5132b743d8891527b35c04f85bf1c5128d6e4
Thabangx10 <Thabangx12@gmail.com>
2023-07-09 04:13:56 +0200

    hey

--- script/admin.js
+++ script/admin.js
@@ -1,49 +1,51 @@
-let products = JSON.parse(localStorage.getItem('products')) ? JSON.parse(localStorage.getItem('products')) : [
-    {
+let products = JSON.parse(localStorage.getItem("products"))
+  ? JSON.parse(localStorage.getItem("products"))
+  : [
+      {
         id: 1,
         title: "Luxury Villa",
         description: "Beautiful villa with stunning views",
         price: 500000,
         image: "https://i.postimg.cc/J7NdV8My/villa.jpg",
-        category: "Luxury"
-    },
-    {
+        category: "Luxury",
+      },
+      {
         id: 2,
         title: "Beachfront Condo",
         description: "Spacious condo right on the beach",
         price: 250000,
         image: "https://i.postimg.cc/fb5Tgpy1/condo.jpg",
-        category: "Beachfront"
-    },
-    {
+        category: "Beachfront",
+      },
+      {
         id: 3,
         title: "Luxury Villa",
         description: "Beautiful villa with stunning views",
         price: 500000,
         image: "https://i.postimg.cc/J7NdV8My/villa.jpg",
-        category: "Suburban"
-    },
-    {
+        category: "Suburban",
+      },
+      {
         id: 4,
         title: "Beachfront Condo",
         description: "Spacious condo right on the beach",
         price: 250000,
         image: "https://i.postimg.cc/fb5Tgpy1/condo.jpg",
-        category: "City"
-    },
-    {
+        category: "City",
+      },
+      {
         id: 5,
         title: "Beachfront Condo",
         description: "Spacious condo right on the beach",
         price: 250000,
         image: "https://i.postimg.cc/fb5Tgpy1/condo.jpg",
-        category: "Beachfront"
-    },
-];
+        category: "Beachfront",
+      },
+    ];
 
 function displayAdmin() {
-    products.forEach((product) => {
-        document.querySelector('.products').innerHTML += `
+  products.forEach((product) => {
+    document.querySelector(".products").innerHTML += `
         <tr>
             <th class="col-2"><span class="Type text-black">${product.id}</span></th>
             <th class="col-2"><span class="Type text-black">${product.category}</span></th>
@@ -60,110 +62,109 @@ function displayAdmin() {
                 </button>
             </span></th>
         </tr>`;
-    });
+  });
 }
 
 function deleteProduct(productId) {
-    products = products.filter((product) => product.id !== productId);
-    updateLocalStorage();
-    refreshTable();
+  products = products.filter((product) => product.id !== productId);
+  updateLocalStorage();
+  refreshTable();
 }
 
 function updateLocalStorage() {
-    localStorage.setItem('products', JSON.stringify(products));
+  localStorage.setItem("products", JSON.stringify(products));
 }
 
 function refreshTable() {
-    document.querySelector('.products').innerHTML = '';
-    displayAdmin();
+  document.querySelector(".products").innerHTML = "";
+  displayAdmin();
 }
 
 displayAdmin();
 
-
 function editProduct(productId) {
-    var product = products.find((p) => p.id === productId);
-    if (product) {
-      var titleInput = document.getElementById("titleInput");
-      var categoryInput = document.getElementById("categoryInput");
-      var priceInput = document.getElementById("priceInput");
-  
-      titleInput.value = product.title;
-      categoryInput.value = product.category;
-      priceInput.value = product.price;
-  
-      // Display save button
-      var saveButton = document.getElementById("saveButton");
-      saveButton.style.display = "inline-block";
-      saveButton.dataset.productId = productId;
-      saveButton.addEventListener("click", saveProductChanges);
-    }
-  }
-  
-  function saveProductChanges(event) {
-    event.preventDefault();
-
-    var productId = parseInt(event.target.dataset.productId);
+  var product = products.find((p) => p.id === productId);
+  if (product) {
     var titleInput = document.getElementById("titleInput");
     var categoryInput = document.getElementById("categoryInput");
     var priceInput = document.getElementById("priceInput");
-  
-    var productIndex = products.findIndex((p) => p.id === productId);
-    if (productIndex !== -1) {
-      products[productIndex].title = titleInput.value;
-      products[productIndex].category = categoryInput.value;
-      products[productIndex].price = parseFloat(priceInput.value);
-  
-      updateLocalStorage();
-      refreshTable();
-      clearInputFields();
-    }
-  
-    // Hide save button
-    event.target.style.display = "none";
 
-    var modal = document.getElementById("editModal");
-    var bootstrapModal = bootstrap.Modal.getInstance(modal);
-    bootstrapModal.hide();
-  }
-  
-  function clearInputFields() {
-    var titleInput = document.getElementById("titleInput");
-    var categoryInput = document.getElementById("categoryInput");
-    var priceInput = document.getElementById("priceInput");
-  
-    titleInput.value = "";
-    categoryInput.value = "";
-    priceInput.value = "";
-  }
+    titleInput.value = product.title;
+    categoryInput.value = product.category;
+    priceInput.value = product.price;
 
-  function addNewProduct(event) {
-    event.preventDefault();
-  
-    var titleInput = document.getElementById("titleInput");
-    var descriptionInput = document.getElementById("descriptionInput");
-    var priceInput = document.getElementById("priceInput");
-    var imageInput = document.getElementById("imageInput");
-    var categoryInput = document.getElementById("categoryInput");
-  
-    var newProduct = {
-      id: products.length + 1,
-      title: titleInput.value,
-      description: descriptionInput.value,
-      price: parseFloat(priceInput.value),
-      image: imageInput.value,
-      category: categoryInput.value,
-    };
-  
-    products.push(newProduct);
+    // Display save button
+    var saveButton = document.getElementById("saveButton");
+    saveButton.style.display = "inline-block";
+    saveButton.dataset.productId = productId;
+    saveButton.addEventListener("click", saveProductChanges);
+  }
+}
+
+function saveProductChanges(event) {
+  event.preventDefault();
+
+  var productId = parseInt(event.target.dataset.productId);
+  var titleInput = document.getElementById("titleInput");
+  var categoryInput = document.getElementById("categoryInput");
+  var priceInput = document.getElementById("priceInput");
+
+  var productIndex = products.findIndex((p) => p.id === productId);
+  if (productIndex !== -1) {
+    products[productIndex].title = titleInput.value;
+    products[productIndex].category = categoryInput.value;
+    products[productIndex].price = parseFloat(priceInput.value);
+
     updateLocalStorage();
     refreshTable();
     clearInputFields();
-  
-    // Hide modal
-    var modal = document.getElementById("addModal");
-    var bootstrapModal = bootstrap.Modal.getInstance(modal);
-    bootstrapModal.hide();
   }
-  
-  document.getElementById("addForm").addEventListener("submit", addNewProduct);
+
+  // Hide save button
+  event.target.style.display = "none";
+
+  var modal = document.getElementById("editModal");
+  var bootstrapModal = bootstrap.Modal.getInstance(modal);
+  bootstrapModal.hide();
+}
+
+function clearInputFields() {
+  var titleInput = document.getElementById("titleInput");
+  var categoryInput = document.getElementById("categoryInput");
+  var priceInput = document.getElementById("priceInput");
+
+  titleInput.value = "";
+  categoryInput.value = "";
+  priceInput.value = "";
+}
+
+function addNewProduct(event) {
+  event.preventDefault();
+
+  var titleInput = document.getElementById("titleInput");
+  var descriptionInput = document.getElementById("descriptionInput");
+  var priceInput = document.getElementById("priceInput");
+  var imageInput = document.getElementById("imageInput");
+  var categoryInput = document.getElementById("categoryInput");
+
+  var newProduct = {
+    id: products.length + 1,
+    title: titleInput.value,
+    description: descriptionInput.value,
+    price: parseFloat(priceInput.value),
+    image: imageInput.value,
+    category: categoryInput.value,
+  };
+
+  products.push(newProduct);
+  updateLocalStorage();
+  refreshTable();
+  clearInputFields();
+
+  // Hide modal
+  var modal = document.getElementById("addModal");
+  var bootstrapModal = bootstrap.Modal.getInstance(modal);
+  bootstrapModal.hide();
+}
+
+document.getElementById("addForm").addEventListener("submit", addNewProduct);
